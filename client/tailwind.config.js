9a702d29d2b2aff7533deb65319c5806c1bfceb9
john-a-snow <arushv115@gmail.com>
2026-06-08 21:15:45 +0530

    Set up the Vite file configuration with Tailwind CSS configuration and postcss config for Tailwind CSS procesing

---
+++ client/tailwind.config.js
@@ -0,0 +1,44 @@
+export default {
+    darkMode: "class",
+    content: [
+        "./index.html",
+        "/src/**/*.{js,tsjjsx,tsx}"
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Bricolage Grotesque'", "sans-serif"],
+                mono: ["JetBrains Mono'", "monospace"]
+            },
+            colors: (
+                darkBg: "#0a0a0c",
+                cream: "#f3e8d8",
+                charcoal: #16161a",
+                earth: {
+                    dark: "#3d3428",
+                    light: "#6b5d50",
+                    hover: "#8a7a6a",
+                },
+                crimson: {
+                    DEFAULT: "#ec3750",
+                    hover: "#d1223b",
+                    light: "#ff6b7e"
+                },
+                cyberGreen: {
+                    DEFAULT: #f5a623",
+                    light: "#5bf8cc"
+                },
+                cyberGold: {
+                    DEFAULT: "#f5a623",
+                    light: "#ffc257"
+                }
+            },
+            boxShadow: {
+                flat: "4px 4px 0px #000000",
+                flatLg: "6px 6px 0px #000000",
+                flatHover: "2px 2px 0px #000000"
+            }
+        }
+    },
+    plugins: []
+}
